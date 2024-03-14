7e7725667cba1e650b1741ee673ea9b08e502c61
rehan zafar <rehanzafar22211@gmail.com>
2024-03-14 11:39:52 +0500

    first commit

---
+++ src/pages/PostForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, PostForm } from "../components/index";
+
+function PostForm() {
+  return (
+    <div className="py-8">
+      <Container>
+        <postForm />
+      </Container>
+    </div>
+  );
+}
+
+export default PostForm;
